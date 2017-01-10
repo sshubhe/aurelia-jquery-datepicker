bfb01e398634bc2511e4cb21e2b63056eef61507
gitKearney <kearney.taaffe@gmail.com>
2017-01-10 05:16:35 -0600

    Instead of using script in the view, using the jquery call in the attach method of the model-view

--- src/routes/calendar/calendar.js
+++ src/routes/calendar/calendar.js
@@ -6,6 +6,10 @@ export class Calendar {
     this.dateSelected = 'blank';
   }
 
+  attached() {
+    $('#datepicker').datepicker();
+  }
+
   submitDateToServer() {
     console.log('SENDING TO SERVER:', this.dateSelected)
   }
